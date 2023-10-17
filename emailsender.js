8e48397adcfc3f6d33a484f645b3d43c0f7d787a
Juhi <tester3.elitetechno @gmail.com>
2023-10-17 17:51:15 +0530

    commit done

--- emailsender.js
+++ emailsender.js
@@ -16,108 +16,76 @@ const transporter = nodemailer.createTransport({
 const mailOptions = {
   from: 'tester3.elitetechno@gmail.com',
   
-  to: 'tester3.elitetechno@gmail.com',
+  //to: '',
+  to: 'yatharth.shah@jainam.biz',
   subject: 'This is Automation Testing Report',
   text: 'Automationn Testing report for Comet web app',
   attachments: [
     {
       filename: 'index.html',
       path: './index.html'
-    },
+    }]
 
-  //   {
-  //     filename: 'Comet_Jainam(failed).png',//failed screenshots
-  //     path: './Comet_Jainam(failed).png',
-  // },
+//   //   {
+//   //     filename: 'Comet_Jainam(failed).png',//failed screenshots
+//   //     path: './Comet_Jainam(failed).png',
+//   // },
 
-  {
-      filename: 'comet.cy.js.mp4' ,
-      path: './comet.cy.js.mp4',
+//   {
+//       filename: 'comet.cy.js.mp4' ,
+//       path: './comet.cy.js.mp4',
 
-  },
+//   },
   
-  {
-    filename: 'comet_login.cy.js.mp4' ,
-    path: './comet_login.cy.js.mp4',
+//   {
+//     filename: 'comet_login.cy.js.mp4' ,
+//     path: './comet_login.cy.js.mp4',
 
-},
-  {
-
-      filename: 'Report_Dividend_Report.cy.js.mp4 ' ,
-      path: './Report_Dividend_Report.cy.js.mp4',
-  },
-
-  {
-
-      filename: 'Report_FNO.cy.js.mp4 ' ,
-      path: './Report_FNO.cy.js.mp4',
-  },
-
-  {
-
-      filename: 'Report_GLobal_Report.cy.js.mp4 ' ,
-      path: './Report_GLobal_Report.cy.js.mp4',
-  },
-
-  {
-
-      filename: 'Report_Ledger.cy.js.mp4' ,
-      path: './Report_Ledger.cy.js.mp4',
-  },
-  {
-
-      filename: 'Report_Portfolio.cy.js.mp4 ' ,
-      path: './Report_Portfolio.cy.js.mp4',
-  },
-
-  {
-
-      filename: 'Report_Tax_Report.cy.js.mp4 ' ,
-      path: './Report_Tax_Report.cy.js.mp4',
-  },
-  {
-
-      filename: 'Report_Tradelog.cy.js.mp4' ,
-      path: './Report_Tradelog.cy.js.mp4',
-  },
-  {
-
-    filename: 'Utilities.cy.js.mp4' ,
-    path: './Utilities.cy.js.mp4',
-},
-
-{
-
-  filename: 'CURRENCY_J33_2021.pdf' ,
-  path: './CURRENCY_J33_2021.pdf',
-},
-
-
-{
-
-  filename: 'EquityScriptwiseGlobalReport_J33_2023.pdf' ,
-  path: './EquityScriptwiseGlobalReport_J33_2023.pdf',
-},
-{
-
-  filename: 'Holding.pdf' ,
-  path: './Holding.pdf',
-},
-// {
-
-//   filename: 'LedgerReport.pdf' ,
-//   path: './LedgerReport.pdf',
 // },
-// {
+//   {
 
-//   filename: 'MCX_J33_2021.pdf' ,
-//   path: './MCX_J33_2021.pdf',
-// },
-{
+//       filename: 'Report_Dividend_Report.cy.js.mp4 ' ,
+//       path: './Report_Dividend_Report.cy.js.mp4',
+//   },
 
-  filename: 'NSE_FNO_J33_2021.pdf' ,
-  path: './NSE_FNO_J33_2021.pdf',
-}]
+//   {
+
+//       filename: 'Report_FNO.cy.js.mp4 ' ,
+//       path: './Report_FNO.cy.js.mp4',
+//   },
+
+//   {
+
+//       filename: 'Report_GLobal_Report.cy.js.mp4 ' ,
+//       path: './Report_GLobal_Report.cy.js.mp4',
+//   },
+
+//   {
+
+//       filename: 'Report_Ledger.cy.js.mp4' ,
+//       path: './Report_Ledger.cy.js.mp4',
+//   },
+//   {
+
+//       filename: 'Report_Portfolio.cy.js.mp4 ' ,
+//       path: './Report_Portfolio.cy.js.mp4',
+//   },
+
+//   {
+
+//       filename: 'Report_Tax_Report.cy.js.mp4 ' ,
+//       path: './Report_Tax_Report.cy.js.mp4',
+//   },
+//   {
+
+//       filename: 'Report_Tradelog.cy.js.mp4' ,
+//       path: './Report_Tradelog.cy.js.mp4',
+//   },
+//   {
+
+//     filename: 'Utilities.cy.js.mp4' ,
+//     path: './Utilities.cy.js.mp4',
+// }]
 
  
 
